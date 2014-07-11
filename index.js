4f08ed32b9d41db0d7c7e956b1aeb441acc662dd
Travis Webb <me@traviswebb.com>
2014-07-10 20:47:37 -0400

    updated package json with colors and bundledDependencies

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var nex = require('nex-api');
 var rimraf = require('rimraf');
 var fs = require('fs');
 var log = require('npmlog');
+var colors = require('colors');
 
 var handler = module.exports = new nex.Handler('symlinks');
 
@@ -12,7 +13,8 @@ var handler = module.exports = new nex.Handler('symlinks');
  * @override
  */
 handler.do = function (pkg) {
-  _.each(pkg.symlinks, function (linkName, target) {
+  _.each(pkg[this.field], function (linkName, target) {
+    log.info('symlink', linkName, '->'.green, target);
     rimraf.sync(linkName);
     fs.symlinkSync(target, linkName);
   });
@@ -22,5 +24,5 @@ handler.do = function (pkg) {
  * @override
  */
 handler.undo = function (pkg) {
-  _.each(pkg.symlinks, rimraf.sync);
+  _.each(pkg[this.field], rimraf.sync);
 };
